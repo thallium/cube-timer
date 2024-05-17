5474cf119efb3fee16bcbed5d3dffcfe5a83ec79
Gengchen <tuogengchen@gmail.com>
2024-05-17 08:37:28 -0600

    test sync

--- src/settings.tsx
+++ src/settings.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@nextui-org/button";
+import PouchDb from "pouchdb-browser";
+import { useState } from "react";
 import { useRegisterSW } from "virtual:pwa-register/react";
 
 function Settings() {
@@ -7,14 +9,12 @@ function Settings() {
     // needRefresh: [needRefresh],
     updateServiceWorker,
   } = useRegisterSW({});
+  const [remoteDB, setRemoteDB] = useState("");
   return (
     <div className="space-y-2 divide-y-2 px-4 text-2xl">
       <div>Offline Ready: {offlineReady ? "Yes" : "No"}</div>
       <div>
-        <Button
-          className="ReloadPrompt-toast-button text-2xl"
-          onClick={() => updateServiceWorker(true)}
-        >
+        <Button className="text-2xl" onClick={() => updateServiceWorker(true)}>
           Reload
         </Button>
       </div>
@@ -22,8 +22,18 @@ function Settings() {
         Remote DB:{" "}
         <input
           type="text"
+          value={remoteDB}
           className="rounded-md outline outline-2 outline-default-400"
+          onChange={(e) => setRemoteDB(e.target.value)}
         />
+        <Button
+          className="inline-block text-2xl"
+          onClick={() => {
+            PouchDb.sync("sessions", "http://admin:password@arm:5900/sessions");
+          }}
+        >
+          Sync
+        </Button>
       </div>
     </div>
   );
