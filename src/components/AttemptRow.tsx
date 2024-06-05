961f52dd9f751dbefbb9adcdab07a12ead98a73d
Gengchen <tuogengchen@gmail.com>
2024-06-05 09:34:09 -0600

    fix attempt row animation issue

--- src/components/AttemptRow.tsx
+++ src/components/AttemptRow.tsx
@@ -40,38 +40,37 @@ function AttemptRow({
         opacity: { duration: t(0.03) },
       }}
       key={_id}
-      className="flex flex-row items-center justify-between border-b-1 py-2"
     >
-      <Button
-        variant="subtle"
-        color="gray.6"
-        className="w-20 px-3 py-2 text-center text-2xl font-normal"
-        onClick={open}
-      >
-        {index + 1}
-      </Button>
-      <Button
-        variant="subtle"
-        color="dark"
-        className="text-2xl font-normal"
-        classNames={{
-          inner: "justify-start",
-        }}
-        onClick={open}
-        fullWidth
-      >
-        <FormattedTime time={totalResultMs} />
-      </Button>
-      {/* <div className="px-3 py-2"> */}
-      <ActionIcon
-        variant="subtle"
-        color="gray.6"
-        size="lg"
-        onClick={() => session.deleteAttempt(_id)}
-      >
-        <X size={24} />
-      </ActionIcon>
-      {/* </div> */}
+      <div className="flex flex-row items-center justify-between border-b-1 py-2">
+        <Button
+          variant="subtle"
+          color="gray.6"
+          className="w-20 text-center text-2xl font-normal"
+          onClick={open}
+        >
+          {index + 1}
+        </Button>
+        <Button
+          variant="subtle"
+          color="dark"
+          className="text-2xl font-normal"
+          classNames={{
+            inner: "justify-start",
+          }}
+          onClick={open}
+          fullWidth
+        >
+          <FormattedTime time={totalResultMs} />
+        </Button>
+        <ActionIcon
+          variant="subtle"
+          color="gray.6"
+          size="lg"
+          onClick={() => session.deleteAttempt(_id)}
+        >
+          <X size={24} />
+        </ActionIcon>
+      </div>
       <Modal
         centered
         opened={opened}
