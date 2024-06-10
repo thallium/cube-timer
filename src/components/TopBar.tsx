9d03a515e6ab23a2ee03b0c58ff22c7ee53d796b
Gengchen <tuogengchen@gmail.com>
2024-06-10 10:55:20 -0600

    Change border color

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -8,7 +8,7 @@ interface TopBarProps {}
 
 const TopBar: React.FC<TopBarProps> = () => {
   return (
-    <header className="fixed inset-0 top-0 flex h-[3.75rem] justify-between border-b border-[var(--mantine-color-text)] px-4">
+    <header className="border-mantine fixed inset-0 top-0 flex h-[3.75rem] justify-between border-b px-4">
       <div className="flex items-center justify-start gap-4">
         <h1 className="text-2xl font-bold">Tl Timer</h1>
         <SessionSwitch />
