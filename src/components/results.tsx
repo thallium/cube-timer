95e6b1ed3b911d0e3cd9c0b097abc04b2c9cab1a
Gengchen <tuogengchen@gmail.com>
2024-06-16 10:42:29 -0400

    Use react-viewport-list to improve results list perf

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,6 +1,8 @@
 import { useSession } from "@/session/useSession";
 import { ScrollArea } from "@mantine/core";
 import { AnimatePresence } from "framer-motion";
+import { useRef } from "react";
+import { ViewportList } from "react-viewport-list";
 import AttemptRow from "./AttemptRow";
 
 function Results({ className }: { className?: string }) {
@@ -8,19 +10,25 @@ function Results({ className }: { className?: string }) {
   const attempts = allAttempts.filter(
     (a) => a.session === currentSession!.name,
   );
+  const ref = useRef<HTMLDivElement | null>(null);
   return (
-    <ScrollArea className={className} type="scroll" scrollHideDelay={300}>
+    <ScrollArea
+      className={className}
+      ref={ref}
+      type="scroll"
+      scrollHideDelay={500}
+    >
       <AnimatePresence initial={false}>
-        {attempts
-          .map((row, index) => (
+        <ViewportList viewportRef={ref} items={attempts.toReversed()}>
+          {(row, index) => (
             <AttemptRow
               key={row._id}
               row={row}
-              index={index}
+              index={attempts.length - 1 - index}
               maxDigits={Math.floor(Math.log10(attempts.length)) + 1}
             />
-          ))
-          .toReversed()}
+          )}
+        </ViewportList>
       </AnimatePresence>
     </ScrollArea>
   );
